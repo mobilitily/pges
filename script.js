ec3f3bc42c881646fcd18fd106d87f1eb9593f76
mobilitily <164016183+mobilitily@users.noreply.github.com>
2024-03-19 10:57:25 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,63 @@
+/* Made By ❤  Nithya Yamasinghe  */
+
+/* Hope you Enjoy my code */
+
+
+// function formValidation() {
+//     var ufname = document.forms["Teacher_signup"]["fname"].value;
+//     var ulname = document.forms["Teacher_signup"]["lname"].value;
+//     var uemail = document.forms["Teacher_signup"]["email"].value;
+//     var upw = document.forms["Teacher_signup"]["pword"].value;
+//     var ucpw = document.forms["Teacher_signup"]["confirm_pword"].value;
+//
+//
+//
+// /**First name validation */
+//     if(ufname == ""){
+//         alert("Error: First Name field required!");
+//         return false;
+//     }
+//
+// /**Last name validation */
+//     if(ulname == ""){
+//         alert("Error: Last Name field required!");
+//         return false;
+//     }
+//
+//
+// /**Email validation */
+//     if(uemail == ""){
+//         alert("Error: Email field required!");
+//         return false;
+//     }
+//
+// /**Check @ sign of email. */
+//     if(uemail !== ""){
+//
+//         var check = uemail.indexOf('@')
+//
+//         if(check < 0 || check == 0){
+//             alert("Error: Please enter valid email address!");
+//             return false;
+//         }
+//
+//     }
+//
+// /**Password validation */
+//     if(upw == ""){
+//         alert("Error: Password field required!");
+//         return false;
+//     }
+//
+// /**confirm password validation */
+//     if(ucpw == ""){
+//         alert("Error: Confirm Password field required!");
+//         return false;
+//     }
+//
+// /**check passwords are match or not*/
+//     if (upw !== ucpw){
+//         alert("Error: Password did not match!");
+//         return false;
+//     }
+// }
